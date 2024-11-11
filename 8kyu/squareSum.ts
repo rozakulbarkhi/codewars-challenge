6e837f08db3b52e51581fe39a837c47caa030b2c
Rozakul Barkhi <rozakulbarkhirb@gmail.com>
2024-11-12 00:31:09 +0700

    feat: squareSum

---
+++ 8kyu/squareSum.ts
@@ -0,0 +1,11 @@
+export function squareSum(numbers: number[]): number {
+  let sum = 0;
+
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i] ** 2;
+  }
+
+  return sum;
+}
+
+console.log(squareSum([1, 2]));
